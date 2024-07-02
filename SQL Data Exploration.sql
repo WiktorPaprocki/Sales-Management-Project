a5a70838a0450894aab7b2866c110ecea0326759
Wiktor Paprocki <125204368+WiktorPaprocki@users.noreply.github.com>
2024-07-02 23:15:44 +0200

    Add files via upload

--- SQL Data Exploration.sql
+++ SQL Data Exploration.sql
@@ -5,40 +5,71 @@ SELECT * FROM DimGeography
 SELECT * FROM FactInternetSales
 
 -- Count of Customers by City
-SELECT geo.City, COUNT(cust.CustomerKey) AS [Count of Customers]
-FROM DimGeography AS geo
-JOIN DimCustomerCleaned AS cust ON geo.GeographyKey = cust.GeographyKey
-GROUP BY City
-ORDER BY [Count of Customers] DESC
+SELECT 
+	geo.City, 
+	COUNT(cust.CustomerKey) AS [Count of Customers]
+FROM 
+	DimGeography AS geo
+JOIN 
+	DimCustomerCleaned AS cust ON geo.GeographyKey = cust.GeographyKey
+GROUP BY 
+	City
+ORDER BY 
+	[Count of Customers] DESC
 
 -- Count of Sales by City
-SELECT geo.City, COUNT(sales.ProductKey) AS [Count of Sales]
-FROM DimGeography AS geo
-JOIN DimCustomerCleaned AS cust ON geo.GeographyKey = cust.GeographyKey
-JOIN FactInternetSales AS sales ON cust.CustomerKey = sales.CustomerKey
-GROUP BY City
-ORDER BY [Count of Sales] DESC
+SELECT 
+	geo.City, 
+	COUNT(sales.ProductKey) AS [Count of Sales]
+FROM 
+	DimGeography AS geo
+JOIN 
+	DimCustomerCleaned AS cust ON geo.GeographyKey = cust.GeographyKey
+JOIN 
+	FactInternetSales AS sales ON cust.CustomerKey = sales.CustomerKey
+GROUP BY 
+	City
+ORDER BY 
+	[Count of Sales] DESC
 
 -- Top 10 Sales by Customer
 WITH CTE_Customer AS 
-(SELECT CustomerKey,  FirstName + ' ' + LastName AS [Full Name]
-FROM DimCustomerCleaned
+(SELECT 
+	CustomerKey,  
+	FirstName + ' ' + LastName AS [Full Name]
+FROM 
+	DimCustomerCleaned
 )
 
-SELECT TOP 10 [Full Name], SUM(sales.SalesAmount) AS [Total Sales Amount]
-FROM CTE_Customer AS cust
-JOIN FactInternetSales AS sales ON cust.CustomerKey = sales.CustomerKey
-GROUP BY [Full Name]
-ORDER BY [Total Sales Amount] DESC
+SELECT TOP 10 
+	[Full Name], 
+	SUM(sales.SalesAmount) AS [Total Sales Amount]
+FROM 
+	CTE_Customer AS cust
+JOIN 
+	FactInternetSales AS sales ON cust.CustomerKey = sales.CustomerKey
+GROUP BY 
+	[Full Name]
+ORDER BY 
+	[Total Sales Amount] DESC
 
 -- Top 10 Most Often Purchasing Customers
 WITH CTE_Customer AS 
-(SELECT CustomerKey,  FirstName + ' ' + LastName AS [Full Name]
-FROM DimCustomerCleaned
+(SELECT 
+	CustomerKey,  
+	FirstName + ' ' + LastName AS [Full Name]
+FROM 
+	DimCustomerCleaned
 )
 
-SELECT TOP 10 [Full Name], COUNT(sales.CustomerKey) AS [Total Orders]
-FROM CTE_Customer AS cust
-JOIN FactInternetSales AS sales ON cust.CustomerKey = sales.CustomerKey
-GROUP BY [Full Name]
-ORDER BY [Total Orders] DESC
+SELECT TOP 10 
+	[Full Name], 
+	COUNT(sales.CustomerKey) AS [Total Orders]
+FROM 
+	CTE_Customer AS cust
+JOIN 
+	FactInternetSales AS sales ON cust.CustomerKey = sales.CustomerKey
+GROUP BY 
+	[Full Name]
+ORDER BY 
+	[Total Orders] DESC
--- SQL table queries.sql
+++ SQL table queries.sql
@@ -1,42 +1,58 @@
 --Dim_Customer Table Query
 
-SELECT cust.customerkey AS CustomerKey, 
-cust.firstname AS [First Name], 
-cust.lastname AS [Last Name], 
-cust.firstname + ' ' + cust.lastname AS [Full Name], 
-cust.datefirstpurchase AS DateFirstPurchase, 
-YearlyIncome,
-CommuteDistance,
-geo.city AS [Customer City]
-
-FROM DimCustomerCleaned AS cust
-LEFT JOIN DimGeography AS geo ON cust.geographyKey = geo.geographyKey
-
-ORDER BY CustomerKey 
+SELECT 
+	cust.customerkey AS CustomerKey, 
+	cust.firstname AS [First Name], 
+	cust.lastname AS [Last Name], 
+	cust.firstname + ' ' + cust.lastname AS [Full Name], 
+	cust.datefirstpurchase AS DateFirstPurchase, 
+	YearlyIncome,
+	CommuteDistance,
+	geo.city AS [Customer City]
+FROM 
+	DimCustomerCleaned AS cust
+LEFT JOIN 
+	DimGeography AS geo ON cust.geographyKey = geo.geographyKey
+ORDER BY 
+	CustomerKey 
 
 --Dim_Products Table Query
 
-SELECT prod.ProductKey, prod.ProductAlternateKey AS [Product Code], 
-prod.EnglishProductName AS [Product Name],
-prod.Color AS [Product Color], 
-prod.Size AS [Product Size], 
-prod.ProductLine AS [Product Line],
-prod.ModelName AS [Product Model Name], 
-prod.EnglishDescription AS [Product Description],
-sub.EnglishProductSubcategoryName AS [Sub Catergory],
-cat.EnglishProductCategoryName AS  [Product Category],
-ISNULL (prod.Status, 'Outdated') AS [Product Status]
-
-FROM DimProduct AS prod
-LEFT JOIN DimProductSubcategory AS sub ON prod.ProductSubcategoryKey = sub.ProductSubcategoryKey
-LEFT JOIN DimProductCategory AS cat ON sub.ProductCategoryKey = cat.ProductCategoryKey
-
-ORDER BY prod.ProductKey
+SELECT 
+	prod.ProductKey, 
+	prod.ProductAlternateKey AS [Product Code], 
+	prod.EnglishProductName AS [Product Name],
+	prod.Color AS [Product Color], 
+	prod.Size AS [Product Size], 
+	prod.ProductLine AS [Product Line],
+	prod.ModelName AS [Product Model Name], 
+	prod.EnglishDescription AS [Product Description],
+	sub.EnglishProductSubcategoryName AS [Sub Catergory],
+	cat.EnglishProductCategoryName AS  [Product Category],
+	ISNULL (prod.Status, 'Outdated') AS [Product Status]
+FROM 
+	DimProduct AS prod
+LEFT JOIN 
+	DimProductSubcategory AS sub ON prod.ProductSubcategoryKey = sub.ProductSubcategoryKey
+LEFT JOIN 
+	DimProductCategory AS cat ON sub.ProductCategoryKey = cat.ProductCategoryKey
+ORDER BY 
+	prod.ProductKey
 
 -- Fact_Internet_Sales Table Query
 
-SELECT ProductKey, OrderDateKey, DueDateKey, ShipDateKey, CustomerKey, SalesOrderNumber, SalesAmount
-FROM FactInternetSales
-WHERE LEFT(OrderDateKey, 4) >= YEAR(GETDATE()) - 2
-ORDER BY OrderDateKey 
+SELECT 
+	ProductKey, 
+	OrderDateKey, 
+	DueDateKey, 
+	ShipDateKey, 
+	CustomerKey, 
+	SalesOrderNumber, 
+	SalesAmount
+FROM 
+	FactInternetSales
+WHERE 
+	LEFT(OrderDateKey, 4) >= YEAR(GETDATE()) - 2
+ORDER BY 
+	OrderDateKey 
 
